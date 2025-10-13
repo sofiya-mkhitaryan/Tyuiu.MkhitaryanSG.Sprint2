a948cf0bf9af10134a61081e114228fc3994edfd
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-13 15:33:54 +0500

    Task4

---
+++ Tyuiu.MkhitaryanSG.Sprint2.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿//если x + 20 > y * 2, то z = x*((y+1)/(x+2))^x иначе y^2 + 2*x + (6/x)
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MkhitaryanSG.Sprint2.Task4.V6.Lib
+{
+    public class DataService : ISprint2Task4V6
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = x + 20 > y * 2 ? x * Math.Pow(((y + 1) / (x + 2)), x) : Math.Pow(y, 2) + 2*x + 6/x;
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint2.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿//если x + 20 > y * 2, то z = x*((y+1)/(x+2))^x иначе y^2 + 2*x + (6/x)
+using Tyuiu.MkhitaryanSG.Sprint2.Task4.V6.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint2.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 4.189;
+            Assert.AreEqual(wait, res);
+
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 20;
+            double res = ds.Calculate(x, y);
+            double wait = 420.6;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
