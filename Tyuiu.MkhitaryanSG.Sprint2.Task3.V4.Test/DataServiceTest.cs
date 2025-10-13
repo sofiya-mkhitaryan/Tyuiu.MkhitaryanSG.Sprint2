58653da84768f4d6b3c56dc41292e282b48d985d
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-13 15:39:18 +0500

    Task3

--- Tyuiu.MkhitaryanSG.Sprint2.Task3.V4.Test/DataServiceTest.cs
+++ Tyuiu.MkhitaryanSG.Sprint2.Task3.V4.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.MkhitaryanSG.Sprint2.Task3.V4.Test
             DataService ds = new DataService();
             double x = 2;
             double res = ds.Calculate(x);
-            double wait = 1;
+            double wait = 11;
             Assert.AreEqual(wait, res);
         }
         [TestMethod]
