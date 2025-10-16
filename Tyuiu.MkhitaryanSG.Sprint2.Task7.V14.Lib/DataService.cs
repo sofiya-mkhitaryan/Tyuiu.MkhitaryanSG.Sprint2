e7ba7c3835c29af3e4e6ee904bf20a028f17c5f8
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-16 20:28:05 +0500

    Task7

---
+++ Tyuiu.MkhitaryanSG.Sprint2.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MkhitaryanSG.Sprint2.Task7.V14.Lib
+{
+    public class DataService : ISprint2Task7V14
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((x * x + y * y <= 1 && y >= x && y <= -x))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint2.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint2.Task7.V14.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint2.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = -0.5;
+            double y = 0;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
